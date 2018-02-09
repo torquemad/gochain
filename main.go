d1a52bc896375850d8f40ed53268e5a174dd9c56
Louis Moore <burntliketoast@gmail.com>
2018-02-09 11:29:04 +1100

    sethash func

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
+package main
+
+import (
+  "strconv"
+  "bytes"
+  "crypto/sha256"
+)
 
 type Block struct {
-	Timestamp			int64
-	Data					[]byte
+	Timestamp     int64
+	Data          []byte
 	PrevBlockHash []byte
-	Hash					[]byte
+	Hash          []byte
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
+
+func main() {
 }

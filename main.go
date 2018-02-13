abe27cd6e5cd9df8ad9f1d2947dd97350ab7350e
Louis Moore <burntliketoast@gmail.com>
2018-02-13 13:35:47 +1100

    ability to created a bc, and add blocks to bc

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "fmt"
   "strconv"
   "bytes"
   "crypto/sha256"
@@ -12,6 +13,10 @@ type Block struct {
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
+}
+
+type BlockChain struct {
+  blocks []*Block
 }
 
 func (b *Block) SetHash() {
@@ -28,5 +33,30 @@ func NewBlock(data string, prevBlockHash []byte) *Block  {
   return block
 }
 
+func (bc *BlockChain) AddBlock(data string) {
+  prevBlock := bc.blocks[len(bc.blocks)-1]
+  newBlock := NewBlock(data, prevBlock.Hash)
+  bc.blocks = append(bc.blocks, newBlock)
+}
+
+func NewGenesisBlock() *Block {
+  return NewBlock("Genesis Block", []byte{})
+}
+
+func NewBlockChain() *BlockChain {
+  return &BlockChain{[]*Block{NewGenesisBlock()}}
+}
+
 func main() {
+  bc := NewBlockChain()
+
+  bc.AddBlock("acc1 1btc to acc2")
+  bc.AddBlock("acc2 5btc to acc1")
+
+  for _, block := range bc.blocks {
+    fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+    fmt.Printf("Data: %s\n", block.Data)
+    fmt.Printf("Hash: %x\n", block.Hash)
+    fmt.Println()
+  }
 }

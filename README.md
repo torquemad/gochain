43e9c43d16fa0a83ed2993d20ee2c3bcc57d5e73
Louis Moore <burntliketoast@gmail.com>
2018-02-15 10:26:27 +1100

    move libs

---
+++ README.md
@@ -0,0 +1,6 @@
+# TODO:
+ - Proof of work - Block hashing algorithm: https://en.bitcoin.it/wiki/Block_hashing_algorithm
+ - Persistance
+ - Transactions
+ - Addresses / wallets
+ - Network
--- main.go
+++ main.go
@@ -2,50 +2,9 @@ package main
 
 import (
   "fmt"
-  "strconv"
-  "bytes"
-  "crypto/sha256"
   "time"
 )
 
-type Block struct {
-	Timestamp     int64
-	Data          []byte
-	PrevBlockHash []byte
-	Hash          []byte
-}
-
-type BlockChain struct {
-  blocks []*Block
-}
-
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-
-	b.Hash = hash[:]
-}
-
-func NewBlock(data string, prevBlockHash []byte) *Block  {
-  block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-  block.SetHash()
-  return block
-}
-
-func (bc *BlockChain) AddBlock(data string) {
-  prevBlock := bc.blocks[len(bc.blocks)-1]
-  newBlock := NewBlock(data, prevBlock.Hash)
-  bc.blocks = append(bc.blocks, newBlock)
-}
-
-func NewGenesisBlock() *Block {
-  return NewBlock("Genesis Block", []byte{})
-}
-
-func NewBlockChain() *BlockChain {
-  return &BlockChain{[]*Block{NewGenesisBlock()}}
-}
 
 func main() {
   bc := NewBlockChain()
@@ -57,6 +16,6 @@ func main() {
     fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
     fmt.Printf("Data: %s\n", block.Data)
     fmt.Printf("Hash: %x\n", block.Hash)
-    fmt.Println()
+    fmt.Printf("Transaction Time: %s\n", time.Unix(block.Timestamp, 0))
   }
 }
